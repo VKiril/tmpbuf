c97f40fce9b917b293a5bdeda7810aed60c0e282
VKiril <everest0corp@gmail.com>
2025-09-23 10:38:03 -0700

    Update bootstrap.sh
    fix order

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,8 +19,10 @@ apt-key add msopentech.asc
 apt-get update
 apt update
 
+apt-get install libtss2-dev
+
 apt install -y kmpp openssl-dev symcrypt libssl-dev libkmpp libkmpp-dev tpm2-tss tpm2-tss-engine-dev dos2unix docker-cli zip unzip mdadm traceroute gdb icu-devel bind-utils bind9-utils icu-dev socat pciutils azsec-clamav azure-security metricsext2 net-tools build-essential cmake protobuf-compiler autoconf libtool pkg-config libsystemd-dev google-perftools libgoogle-perftools-dev libcryptsetup-dev git curl zlib1g-dev libcurl4-openssl-dev libabseil-cpp-dev libboost-all-dev libabsl-dev uuid-dev uuid-de
-apt-get install -y curl gpg apt-transport-https azure-mdsd clamav libjson-c-dev libtss2-dev azsec-monitor
+apt-get install -y curl gpg apt-transport-https azure-mdsd clamav libjson-c-dev azsec-monitor
 
 echo "aplinux ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/aplinux
 
